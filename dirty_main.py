ec1ef24126762c10435d5702533c05416b390b0e
Rus <islam06.95@mail.ru>
2023-09-16 22:51:55 +0300

    Add

---
+++ dirty_main.py
@@ -0,0 +1,7 @@
+from application.salary import *
+from application.db.people import *
+from datetime import datetime
+if __name__ == '__main__':
+    print(datetime.now())
+    calculate_salary()
+    get_employees()
